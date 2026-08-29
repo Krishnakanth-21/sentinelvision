054051d4219081e80853daf944baed72abbd7932
Krishnakanth-21 <krishnakanthmohanraj21@gmail.com>
2026-08-29 22:37:40 +1000

    Prevent duplicate image metadata inserts

--- src/load_to_postgres.py
+++ src/load_to_postgres.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+
 
 engine = create_engine(
     "postgresql+psycopg2://krishnakanthmohanraj@localhost:5432/sentinelvision"
@@ -11,9 +12,26 @@ metadata_df = pd.read_csv(metadata_path)
 print(f"Total images processed: {len(metadata_df)}")
 
 with engine.connect() as connection:
-    print("Connected to the database successfully.")
+    existing_hashes = pd.read_sql(
+        text("SELECT sha256 FROM image_metadata WHERE sha256 IS NOT NULL"), connection)
 
-metadata_df.to_sql(
-    "image_metadata", engine, if_exists="replace", index=False)
+existing_hashes_set = set(existing_hashes["sha256"])
+print(f"Existing hashes in the database: {len(existing_hashes_set)}")
 
-print("Data loaded into the database successfully.")
+new_metadata_df = metadata_df[
+    ~metadata_df["sha256"].isin(existing_hashes_set)
+]
+
+print(f"New images to insert: {len(new_metadata_df)}")
+
+
+if len(new_metadata_df) > 0:
+    new_metadata_df.to_sql(
+        "image_metadata",
+        engine,
+        if_exists="append",
+        index=False
+    )
+    print("New image metadata inserted successfully.")
+else:
+    print("No new images to insert.")
